fa2092e203a73d0840a9bcdce6699c10a2cd81e5
Steve Babb <steve.babb@outlook.com>
2021-04-08 15:04:08 -0500

    add total duration to exercises

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -4,19 +4,44 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
-  exercises: [{
-    type: {
-      type: String
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
     },
-    name: String,
-    duration: Number,
-    distance: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
-  }]
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- Develop/server.js
+++ Develop/server.js
@@ -5,32 +5,63 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
+const e = require("express");
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
+// route to get latest workout
+// app.get("/api/workouts", async (req, res) => {
+//     try {
+//         const lastWorkout = await db.Workout.find({})
+//         res.send(lastWorkout);
+//     } catch (error) {
+//         res.json(error)
+//     }
+// });
+
+// route to get latest workout with total duration
 app.get("/api/workouts", async (req, res) => {
-    try {
-        const workouts = await db.Workout.find({});
-        // const workoutdata = await workouts.json();
-        res.send(workouts);
-    } catch (error) {
-        res.json(error)
-    }
+  try {
+    const lastWorkout = await db.Workout.find({});
+
+    lastWorkout.forEach((workout) => {
+      let total = 0;
+      workout.exercises.forEach((e) => {
+        total += e.duration;
+      });
+      workout.totalDuration = total;
+    });
+    res.json(lastWorkout);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// route all workouts in range (SHOULD BE RETURNING DATA FOR DASHBOARD BUT ROUTE NOT WORKING)
+app.get("/api/workouts/range", async (req, res) => {
+  try {
+    const inRange = await db.Workout.find({});
+    res.json(inRange);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
