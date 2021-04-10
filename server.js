b2b34e081007a46008c549815a4fe30520ce3185
Steve Babb <steve.babb@outlook.com>
2021-04-10 09:40:01 -0500

    update comments

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+//importing required dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
+//creating a port
 const PORT = process.env.PORT || 3001;
 
 const db = require("./models");
@@ -21,6 +23,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//setting up mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -98,6 +101,7 @@ app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
+//spinning up server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
